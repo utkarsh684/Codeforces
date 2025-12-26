9d006782de6056513698a87b042fb03ab6dd8ed4
utkarsh684 <shuklautkarsh406@gmail.com>
2025-12-26 13:45:06 +0530

    done doubly ended substring

--- src/main/java/Rated_1000_questions/DoubleEndedStrings.java
+++ src/main/java/Rated_1000_questions/DoubleEndedStrings.java
@@ -1,4 +1,60 @@
 package Rated_1000_questions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class DoubleEndedStrings {
+
+    public static void main(String args[]) throws IOException {
+        FastScanner fs = new FastScanner();
+        int t = fs.nextInt();
+        while(t-->0){
+            String a = fs.next();
+            String b = fs.next();
+
+            int ans = longestCommonSubstring(a, b);
+            System.out.println((a.length()-ans) + (b.length()-ans));
+        }
+    }
+
+    public static int longestCommonSubstring(String s, String t) {
+        int n = s.length(), m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    private static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
+
 }
