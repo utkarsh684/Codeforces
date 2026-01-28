7f3e462807a8d87afc4465e59b8af526da4fe029
utkarsh684 <shuklautkarsh406@gmail.com>
2026-01-28 13:14:32 +0530

    done sort the subarray

--- src/main/java/Rated_1100_questions/LunaticNeverContent.java
+++ src/main/java/Rated_1100_questions/LunaticNeverContent.java
@@ -1,4 +1,35 @@
 package Rated_1100_questions;
 
+import java.util.*;
 public class LunaticNeverContent {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+
+            long[] a = new long[n];
+            for(int i=0;i<n;i++){
+                a[i] = sc.nextLong();
+            }
+
+            long ans = 0;
+            for(int i=0;i<n;i++){
+                ans = gcd(ans, Math.abs(a[i] - a[n-i-1]));
+            }
+            System.out.println(ans);
+        }
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+
 }
--- src/main/java/Rated_1100_questions/SortTheSubarray.java
+++ src/main/java/Rated_1100_questions/SortTheSubarray.java
@@ -1,4 +1,62 @@
 package Rated_1100_questions;
 
+import java.util.*;
 public class SortTheSubarray {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t =sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            long[] a = new long[n];
+            long[] b = new long[n];
+            for(int i=0;i<n;i++){
+                a[i] = sc.nextInt();
+            }
+
+            for(int i=0;i<n;i++){
+                b[i] = sc.nextInt();
+            }
+
+            int l = -1;
+            int r = -1;
+            for(int i=0;i<n;i++){
+                if(a[i] != b[i]){
+                    l=i;
+                    break;
+                }
+            }
+
+            for(int i=n-1;i>=0;i--){
+                if(a[i] != b[i]){
+                    r= i;
+                    break;
+                }
+            }
+
+            int l2 = l;
+            for(int i=l2;i>0;i--){
+                if(b[i-1] <= b[i] ){
+                    if(b[i-1] == a[i-1]){
+                        l--;
+                    }
+                } else{
+                    break;
+                }
+            }
+
+            int r2 = r;
+            for(int i=r2;i<n-1;i++){
+                if(b[i+1]>=b[i]){
+                    if(b[i+1] == a[i+1]){
+                        r++;
+                    }
+                } else{
+                    break;
+                }
+            }
+            System.out.println((l+1)+" "+(r+1));
+        }
+    }
+
 }
