e44d65406cc061fabed1e716b4725ec696dde0e9
utkarsh684 <shuklautkarsh406@gmail.com>
2026-06-29 23:03:50 +0530

    done coprime

--- src/main/java/Rated_1100_questions/CoPRIME.java
+++ src/main/java/Rated_1100_questions/CoPRIME.java
@@ -3,21 +3,47 @@ package Rated_1100_questions;
 import java.util.*;
 public class CoPRIME {
 
+    static List<Integer>[] pairs = new ArrayList[1001];
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        for(int i=1;i<=1000;i++){
+            pairs[i] = new ArrayList<>();
+            for (int j = 1; j <= 1000; j++) {
+                if (gcd(i, j) == 1) {
+                    pairs[i].add(j);
+                }
+            }
+        }
         int t = sc.nextInt();
         while(t-->0){
             int n = sc.nextInt();
-            int[] a = new int[n];
             int[] lastSeen = new int[1001];
-            for(int i=0;i<n;i++){
-                a[i] = sc.nextInt();
-                lastSeen[a[i]] = i+1;
+            for(int i=1;i<=n;i++){
+                int x = sc.nextInt();
+                lastSeen[x] = i;
             }
-            for(int i=0;i<=1000;i++){
-                
+            int ans = -1;
+            for(int i=1;i<=1000;i++){
+                if(lastSeen[i] == 0) continue;
+
+                for(int j : pairs[i]){
+                    if(lastSeen[j] != 0){
+                        ans = Math.max(ans, lastSeen[i]+lastSeen[j]);
+                    }
+                }
             }
+            System.out.println(ans);
         }
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 
 }
