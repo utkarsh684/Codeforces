4c60ef5a464b0d7b3b0af979d656642c5f5e862c
utkarsh684 <shuklautkarsh406@gmail.com>
2026-06-30 13:22:52 +0530

    done virus

--- src/main/java/Rated_1200_questions/Virus.java
+++ src/main/java/Rated_1200_questions/Virus.java
@@ -3,70 +3,37 @@ package Rated_1200_questions;
 import java.util.*;
 public class Virus {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t-->0){
             int n = sc.nextInt();
-            char[][] grid = new char[n][n];
-
-            for (int i = 0; i < n; i++) {
-                String row = sc.next();
-                for (int j = 0; j < n; j++) {
-                    grid[i][j] = row.charAt(j);
+            int m = sc.nextInt();
+            int[] a = new int[m];
+            for(int i=0;i<m;i++){
+                a[i] = sc.nextInt();
+            }
+            Arrays.sort(a);
+            List<Integer> gaps = new ArrayList<>();
+            for(int i=0;i<m-1;i++){
+                gaps.add(a[i+1]-a[i]-1);
+            }
+            gaps.add(a[0] + n - a[m-1] - 1);
+            Collections.sort(gaps, Collections.reverseOrder());
+            int saved = 0;
+            int days = 0;
+            for(int gap : gaps){
+                int currGap = gap - 2*days;
+                if(currGap > 0){
+                    saved ++;
+                    currGap -= 2;
+                    if(currGap > 0){
+                        saved += currGap;
+                    }
+                    days += 2;
                 }
             }
-
-            int ans = 0;
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    int ones = 0;
-                    int zeros = 0;
-
-                    if(grid[i][j] == '0'){
-                        zeros++;
-                    } else{
-                        ones++;
-                    }
-
-                    if(grid[j][n-i-1] == '0'){
-                        zeros++;
-                    } else{
-                        ones++;
-                    }
-
-                    if(grid[n-i-1][n-j-1] == '0'){
-                        zeros++;
-                    } else{
-                        ones++;
-                    }
-
-                    if(grid[n-j-1][i] == '0'){
-                        zeros++;
-                    } else{
-                        ones++;
-                    }
-
-                    if(ones == 0 || zeros == 0){
-                        continue;
-                    }
-
-                    if(ones >= zeros){
-                        ans += zeros;
-                        grid[i][j] = '1';
-                        grid[j][n-i-1] = '1';
-                        grid[n-i-1][n-j-1] = '1';
-                        grid[n-j-1][i] = '1';
-                    } else{
-                        ans += ones;
-                        grid[i][j] = '0';
-                        grid[j][n-i-1] = '0';
-                        grid[n-i-1][n-j-1] = '0';
-                        grid[n-j-1][i] = '0';
-                    }
-                }
-            }
-            System.out.println(ans);
+            System.out.println(n-saved);
         }
     }
 
