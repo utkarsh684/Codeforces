dd20a8647f22020b45dec9fd0129e0080e73b8f2
utkarsh684 <shuklautkarsh406@gmail.com>
2026-07-14 12:55:12 +0530

    done

---
+++ src/main/java/RemovevomeR.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class RemovevomeR {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            String s = sc.next();
+            int count=0;
+            for(int i=0;i<n-1;i++){
+                if(s.charAt(i) != s.charAt(i+1)) count++;
+            }
+            System.out.println(count == 1 ? 2 : 1);
+        }
+    }
+}
